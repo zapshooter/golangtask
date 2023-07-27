21f8b54b9668ebd001c1ce49a259e4d303c78456
Anuj <anujgusain2001@gmail.com>
2023-07-27 13:16:24 +0530

    latest update

--- main.go
+++ main.go
@@ -1,111 +1,170 @@
 package main
 
 import (
+	"database/sql"
 	"log"
-	"net/http"
-	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/yourusername/task_management_api/models"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-func checkErr(err error) {
+type Task struct {
+	ID          int64  `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Due_Date    string `json:"due_date"`
+	Status      string `json:"status"`
+}
+
+var db *sql.DB
+
+func main() {
+	// Connecting to sqlite database
+	var err error
+	db, err = sql.Open("sqlite3", "./taskk.db")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error opening database:", err)
+	}
+	defer db.Close()
+
+	// Creating the "taskk" table if it doesn't exist
+	createTable()
+
+	// Initialize the Gin router
+	r := gin.Default()
+
+	// Define routes for CRUD operations
+	r.POST("/taskk", createTask)
+	r.GET("/taskk/:id", getTaskbyid)
+	r.GET("/taskk", gettaskk)
+	r.PUT("/taskk/:id", updateTask)
+	r.DELETE("/taskk/:id", deleteTask)
+
+	// Start the server
+	r.Run(":8000")
+}
+func createTable() {
+	query := `
+	CREATE TABLE IF NOT EXISTS taskk (
+		id INTEGER PRIMARY KEY,
+		title TEXT NOT NULL,
+		description TEXT NOT NULL,
+		due_date DATE NOT NULL,
+		status TEXT NOT NULL
+	);`
+
+	_, err := db.Exec(query)
+	if err != nil {
+		log.Fatal("Error in creating a table:", err)
 	}
 }
 
-func getTasks(c *gin.Context) {
-
-	tasks, err := models.GetTasks()
-	checkErr(err)
-	if tasks == nil {
-		c.JSON(http.StatusBadRequest, gin.H{"message": "no record found"})
+func createTask(c *gin.Context) {
+	var task Task
+	if err := c.ShouldBindJSON(&task); err != nil {
+		c.JSON(400, gin.H{"error": "Invalid data"})
 		return
-	} else {
-		c.IndentedJSON(http.StatusOK, gin.H{"data": tasks})
 	}
+	if task.Title == "" || task.Description == "" || task.Due_Date == "" || task.Status == "" {
+		c.JSON(400, gin.H{"error": "Missing required fields"})
+		return
+	}
+
+	task.ID = time.Now().UnixNano() / int64(time.Millisecond)
+
+	stmt, err := db.Prepare("INSERT INTO taskk (id, title, description, due_date, status) VALUES (?, ?, ?, ?, ?)")
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Internal Error"})
+		return
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(task.ID, task.Title, task.Description, task.Due_Date, task.Status)
+	if err != nil {
+		c.JSON(500, gin.H{"error": "something went wrong"})
+		return
+	}
+
+	c.JSON(201, task)
 }
-func getTaskById(c *gin.Context) {
+
+func getTaskbyid(c *gin.Context) {
+	var task Task
 	id := c.Param("id")
 
-	task, err := models.GetTaskById(id)
-	checkErr(err)
-
-	if task.Title == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "no recoeds found"})
+	row := db.QueryRow("SELECT id, title, description, due_date, status FROM taskk WHERE id = ?", id)
+	err := row.Scan(&task.ID, &task.Title, &task.Description, &task.Due_Date, &task.Status)
+	if err != nil {
+		c.JSON(404, gin.H{"error": "Task not found"})
 		return
-	} else {
-		c.JSON(http.StatusOK, gin.H{"data": task})
 	}
+
+	c.JSON(200, task)
 }
 
-func addTask(c *gin.Context) {
+func gettaskk(c *gin.Context) {
+	var taskk []Task
 
-	var json models.Task
-
-	if err := c.ShouldBindJSON(&json); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	rows, err := db.Query("SELECT id, title, description, due_date, status FROM taskk")
+	if err != nil {
+		c.JSON(500, gin.H{"error": "something went wrong"})
 		return
 	}
+	defer rows.Close()
 
-	success, err := models.AddTask(json)
-
-	if success {
-		c.JSON(http.StatusOK, gin.H{"message": "Success", "data": json})
-	} else {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+	for rows.Next() {
+		var task Task
+		err := rows.Scan(&task.ID, &task.Title, &task.Description, &task.Due_Date, &task.Status)
+		if err != nil {
+			c.JSON(500, gin.H{"error": "something went wrong"})
+			return
+		}
+		taskk = append(taskk, task)
 	}
 
+	c.JSON(200, taskk)
 }
 
 func updateTask(c *gin.Context) {
-	var json models.Task
-
-	if err := c.ShouldBindJSON(&json); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	id := c.Param("id")
+	var task Task
+	if err := c.ShouldBindJSON(&task); err != nil {
+		c.JSON(400, gin.H{"error": "Invalid data"})
 		return
 	}
-	taskId, err := strconv.Atoi(c.Param("id"))
 
+	stmt, err := db.Prepare("UPDATE taskk SET title = ?, description = ?, due_date = ?, status = ? WHERE id = ?")
 	if err != nil {
-		c.JSON(http.StatusBadGateway, gin.H{"error": "invalid id"})
+		c.JSON(500, gin.H{"error": "Something went wrong"})
+		return
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(task.Title, task.Description, task.Due_Date, task.Status, id)
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Something went wrong"})
+		return
 	}
 
-	success, err := models.UpdateTask(json, taskId)
-	if success {
-		c.JSON(http.StatusOK, gin.H{"message": "success", "data": json})
-	} else {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err})
-	}
+	c.JSON(200, task)
 }
 
 func deleteTask(c *gin.Context) {
-	taskId, err := strconv.Atoi(c.Param("id"))
+	id := c.Param("id")
+
+	stmt, err := db.Prepare("DELETE FROM taskk WHERE id = ?")
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"message": "invalid id"})
+		c.JSON(500, gin.H{"error": "Something went wrong"})
+		return
 	}
-	success, err := models.DeleteTask(taskId)
+	defer stmt.Close()
 
-	if success {
-		c.JSON(http.StatusOK, gin.H{"message": "Success"})
-	} else {
-		c.JSON(http.StatusBadRequest, gin.H{"message": err})
+	_, err = stmt.Exec(id)
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Something Went wrong"})
+		return
 	}
-}
 
-func main() {
-	err := models.ConnectDatabase()
-	checkErr(err)
-
-	router := gin.Default()
-
-	router.GET("task", getTasks)
-	router.GET("task/:id", getTaskById)
-	router.POST("task", addTask)
-	router.PUT("task/:id", updateTask)
-	router.DELETE("task/:id", deleteTask)
-
-	router.Run("localhost:8080")
-
+	c.JSON(200, gin.H{"message": "Task deleted successfully"})
 }
